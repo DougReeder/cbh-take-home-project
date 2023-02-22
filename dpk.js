b2446d7eccbde33e05c4c8f86f35ca2772879b2a
P. Douglas Reeder <reeder.29@gmail.com>
2023-02-22 15:52:42 -0500

    refactors

--- dpk.js
+++ dpk.js
@@ -5,22 +5,21 @@ exports.deterministicPartitionKey = (event) => {
   const MAX_PARTITION_KEY_LENGTH = 256;
   let candidate;
 
-  if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
+  if (!event) {
+    return TRIVIAL_PARTITION_KEY;
   }
 
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
+  if (event.partitionKey) {
+    if ("string" === typeof event.partitionKey) {
+      candidate = event.partitionKey;
+    } else {
+      candidate = JSON.stringify(event.partitionKey);
     }
   } else {
-    candidate = TRIVIAL_PARTITION_KEY;
+    const data = JSON.stringify(event);
+    candidate = crypto.createHash("sha3-512").update(data).digest("hex");
   }
+
   if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
     candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
   }
