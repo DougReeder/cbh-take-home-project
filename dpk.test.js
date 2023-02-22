f389c7440f1b6e4b997885cbb9aff4330dfa84b9
P. Douglas Reeder <reeder.29@gmail.com>
2023-02-22 15:41:07 -0500

    Unit tests of existing behavior

--- dpk.test.js
+++ dpk.test.js
@@ -6,8 +6,10 @@ describe("deterministicPartitionKey", () => {
     expect(trivialKey).toBe("0");
   });
 
-  it("Returns the partition key when given a partition key", () => {
-    const PARTITION_KEY = "999";
+  it("Returns the partition key when given a partition key no longer than 256 chars", () => {
+    const array = new Array(256);
+    array.fill('A');
+    const PARTITION_KEY = array.join("");
     const event = {partitionKey: PARTITION_KEY}
     const key = deterministicPartitionKey(event);
     expect(key).toBe(PARTITION_KEY);
@@ -33,5 +35,11 @@ describe("deterministicPartitionKey", () => {
     const event = {partitionKey: PARTITION_KEY}
     const key = deterministicPartitionKey(event);
     expect(key.length).toBe(128);
+  });
+
+  it("returns hashed value when not passed partition key", () => {
+    const event = 42;
+    const key = deterministicPartitionKey(event);
+    expect(key).toBe("4e94ccd8d7dc0381681ba14408f5a4f7a9834d0101b1e21db1396f9bf431c852a5a3eabd3aeb6195ff3d490625a6ea75d0a7fc3761b20e1fdbc57bd0758286dc");
   });
 });
